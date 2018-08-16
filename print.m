ac4021524ecf81cb65b4eabd71ddc3a78e9c5507
Daniel Santos <dfsantosbu@unal.edu.co>
2018-08-16 07:45:48 -0400

    Add function to print the line.

--- print.m
+++ print.m
@@ -1,3 +1,14 @@
-function hello:
-    print("Hello World")
+res = hello( "hello world " );
+disp( res ) 
+function res =  hello( txt )
+    disp( txt )
+    res = computeValue( 10 );
 end
+
+function res = computeValue( val )
+   res = val * 2;
+end
+
+
+
+
