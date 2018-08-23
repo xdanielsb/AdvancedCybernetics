f7906cdcf2b375f2fe46988c5e82ba79372d4400
Daniel Santos <dfsantosbu@unal.edu.co>
2018-08-23 18:05:44 -0400

    Improve boolean control.

--- task/task1/exercise2/booleanControl.m
+++ task/task1/exercise2/booleanControl.m
@@ -1,14 +1,25 @@
-function ans = booleanControl( u )
-    if u > 1 &&  u <= 2
-        error = 1
-    elseif u >2 && u <= 4
-        error = 1
-    elseif u >=-2 && u <=0
-        error = 1
-    elseif u >=-4 && u <= -2
-        error = 1
-    else 
-        error = 0
-    end
-    ans = u - error
+function ans = booleanControl( u, una, unb, unc, und )
+
+ A = u > 1 &&  u <= 2
+ B = u > 2 && u <= 4
+ C = u >=-2 && u <=0
+ D = u >=-4 && u <= -2
+  
+ F1 = or( neg(C) , A);
+ F2 = neg( C ) ;
+ F3 = neg( D );
+
+ ans =  una*F1 + unb*F2 + unc*F3
+end
+
+function res = neg( a)
+  res = 1 - a;
+end
+
+function res = or( a, b)
+  res = max( a, b);
+end
+
+function res = and(a,b)
+ res = min( a,b );
 end
