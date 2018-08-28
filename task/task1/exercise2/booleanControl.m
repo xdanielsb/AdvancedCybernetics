66dcaea6839ad9857d6de33fd311cf889674b49b
Daniel Santos <dfsantosbu@unal.edu.co>
2018-08-28 08:41:58 -0400

    Add solution 2nd exercise.

--- task/task1/exercise2/booleanControl.m
+++ task/task1/exercise2/booleanControl.m
@@ -1,15 +1,18 @@
-function ans = booleanControl( u, una, unb, unc, und )
+function ans = booleanControl( u, una, unb, unc, und, uz )
 
- A = u > 1 &&  u <= 2
- B = u > 2 && u <= 4
- C = u >=-2 && u <=0
- D = u >=-4 && u <= -2
-  
- F1 = or( neg(C) , A);
- F2 = neg( C ) ;
- F3 = neg( D );
-
- ans =  una*F1 + unb*F2 + unc*F3
+ 
+ A = u >= 0.0 &&  u < 0.25;
+ B = u >= 0.25; % &&  u <= 4;
+ C = u >-0.25 &&  u < 0;
+ D = u <=-0.25;% u>=-4 &&  u <= -2;
+ 
+ ans =  A*una + B*unb + C*unc + D*und +uz ;
+ if( u == 0) 
+   ans = u
+ end
+ %disp(u);
+ %fprintf("A = %f, B =%f, C=%f, D=%f, ans =%f\n", A, B, C, D, ans);
+    % una*F1 + unb*F2 + unc*F3
 end
 
 function res = neg( a)
--- task/task1/exercise2/taks.m
+++ task/task1/exercise2/taks.m
@@ -6,3 +6,20 @@ clc; clear all; close all; format compact;
 
 %Defining variables 
 K = 8; b1=3; b0 = 3;
+
+H = 1;
+%Initial Values
+A = [-1/8, -1/4, 1, 1/8].*H*-1
+uZ =0.3; 
+uA =A(1); uB = A(2); uC = A(3); uD = A(4); 
+
+%Test behaviour 
+from = -5; to = 5; N = 100;
+levels = linspace(from,to, N);
+
+for i =  1:N
+  rate(i) = booleanControl( levels(i), uA, uB , uC, uD, uZ);
+end
+
+%Unit Testing 
+plot( levels, rate);
