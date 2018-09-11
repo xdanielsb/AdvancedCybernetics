cb1bb94a954bf5b838999f09ad6b40ed1147a8a1
Daniel Santos <dfsantosbu@unal.edu.co>
2018-09-11 19:43:37 -0400

    Create progress in exercise 9.

--- task/task1/exercise9/sol.m
+++ task/task1/exercise9/sol.m
@@ -7,35 +7,35 @@ b0 = 0.005; b1 = 0.01; b2 = 0.01;
 
 sys = newfis('fstate');
 
-K = 100;
+K = 1;
 %input error
-sys = addvar(sys,'input','error',[-1 1]*K);
+sys = addvar(sys,'input','error',[-1 1]);
  % activation function
- sys = addmf(sys,'input',1,'N','zmf',  [-0.6463 -0.3561]*K);
- sys = addmf(sys,'input',1,'Z','gaussmf',  [0.214 0]*K);
- sys = addmf(sys,'input',1,'P','smf',   [0.3274 0.6377]*K);
+ sys = addmf(sys,'input',1,'N','zmf',   [-0.938 -0.0301]);
+ sys = addmf(sys,'input',1,'Z','gaussmf',   [0.214 0]);
+ sys = addmf(sys,'input',1,'P','smf',     [0.01807 0.881]);
  
 %input derivate error
-sys = addvar(sys,'input','derror',[-1 1]*K);
+sys = addvar(sys,'input','derror',[-1 1]);
  % activation function
- sys = addmf(sys,'input',2,'N','gaussmf',[0.177 -0.733]*K);
- sys = addmf(sys,'input',2,'Z','gaussmf',  [0.214 0]*K);
- sys = addmf(sys,'input',2,'P','gaussmf',  [0.1711 0.581]*K);
+ sys = addmf(sys,'input',2,'N','zmf',   [-0.938 -0.0301]);
+ sys = addmf(sys,'input',2,'Z','gaussmf',   [0.214 0]);
+ sys = addmf(sys,'input',2,'P','smf',     [0.01807 0.881]);
  
 
 %output
-sys = addvar(sys, 'output', 'rate', [-0.5 0.5]*K);
+sys = addvar(sys, 'output', 'rate', [-1 1]);
  % activation function 
- sys = addmf(sys,'output',1,'N','gaussmf',  [0.1705 -0.5]*K);
- sys = addmf(sys,'output',1,'Z','gaussmf',  [0.08016 0]*K);
- sys = addmf(sys,'output',1,'P','gaussmf',  [0.1705 0.5]*K);
+ sys = addmf(sys,'output',1,'N','gaussmf',  [0.1705 -0.5]);
+ sys = addmf(sys,'output',1,'Z','gaussmf',  [0.08016 0]);
+ sys = addmf(sys,'output',1,'P','gaussmf',  [0.1705 0.5]);
  
 %create rules
 % error, derror, output
  rules = [
    1 1 1 1 1
    1 2 1 1 1
-   1 3 2 1 1
+   1 3 1 1 1
    2 1 1 1 1
    2 2 2 1 1
    2 3 3 1 1
@@ -45,4 +45,4 @@ sys = addvar(sys, 'output', 'rate', [-0.5 0.5]*K);
  ];
 
 sys = addrule( sys, rules);
-
+%fuzzy(sys);
