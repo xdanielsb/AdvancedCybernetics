9e5796ea584586d3b4859046a5d2b4968f8db45b
Daniel Santos <dfsantosbu@unal.edu.co>
2018-09-13 17:48:18 -0400

    Add solution exercise 9 and create simulation exercise 10.

--- task/task1/exercise9/sol.m
+++ task/task1/exercise9/sol.m
@@ -8,35 +8,35 @@ num = [b0 b1 b2];
 den = [1 -1.8 0.82];
 
 G = tf(num, den)
-%stepplot(G);
+stepplot(G);
 
 
 sys = newfis('fstate');
 
 K = 1;
 %input error
-sys = addvar(sys,'input','error',[-1 1]);
+sys = addvar(sys,'input','error', [-0.2 0.2]);
  % activation function
- sys = addmf(sys,'input',1,'N','zmf',   [-1 -0.0301]);
- sys = addmf(sys,'input',1,'Z','gaussmf',   [0.214 0]);
- sys = addmf(sys,'input',1,'P','smf',     [0.01807 1]);
+ sys = addmf(sys,'input',1,'N','zmf',    [-0.1913 0.03771]);
+ sys = addmf(sys,'input',1,'Z','gaussmf',   [0.06968 0]);
+ sys = addmf(sys,'input',1,'P','smf',      [0.003616 0.2]);
  
 %input derivate error
-sys = addvar(sys,'input','derror',[-1 1]);
+sys = addvar(sys,'input','derror', [-0.5 0.5]);
  % activation function
- sys = addmf(sys,'input',2,'N','zmf',   [-1 -0.0301]);
- sys = addmf(sys,'input',2,'Z','gaussmf',   [0.214 0]);
- sys = addmf(sys,'input',2,'P','smf',     [0.01807 1]);
+ sys = addmf(sys,'input',2,'N','zmf',    [-0.4877 0.08233]);
+ sys = addmf(sys,'input',2,'Z','gaussmf',    [0.1863 0.0306]);
+ sys = addmf(sys,'input',2,'P','smf',      [0.009035 0.5]);
  
 
 %output
-sys = addvar(sys, 'output', 'rate', [-1 1]);
+sys = addvar(sys, 'output', 'rate',  [-10 10]);
  % activation function 
- sys = addmf(sys,'output',1,'MN','gaussmf',   [0.171 -0.6133]   );
- sys = addmf(sys,'output',1,'N','gaussmf',    [0.07584 -0.234]);
- sys = addmf(sys,'output',1,'Z','gaussmf',  [0.08016 0]);
- sys = addmf(sys,'output',1,'P','gaussmf',   [0.065 0.2387]);
- sys = addmf(sys,'output',1,'MP','gaussmf',   [0.171 0.7561]);
+ sys = addmf(sys,'output',1,'MN','gaussmf',     [1.96 -5.06] );
+ sys = addmf(sys,'output',1,'N','gaussmf',      [1.36 -3.442]);
+ sys = addmf(sys,'output',1,'Z','gaussmf',   [1.061 -1.71]);
+ sys = addmf(sys,'output',1,'P','gaussmf',    [0.9 1.777]);
+ sys = addmf(sys,'output',1,'MP','gaussmf',     [1.71 4.05]);
  
 %create rules
 % error, derror, output
@@ -44,14 +44,14 @@ sys = addvar(sys, 'output', 'rate', [-1 1]);
  rules = [
    1 1 5 1 1
    1 2 4 1 1
-   1 3 4 1 1
+   1 3 3 1 1
    2 1 4 1 1
    2 2 3 1 1
    2 3 2 1 1
-   3 1 2 1 1
+   3 1 3 1 1
    3 2 2 1 1
    3 3 1 1 1
  ];
 
 sys = addrule( sys, rules);
-%fuzzy(sys);
+fuzzy(sys);
