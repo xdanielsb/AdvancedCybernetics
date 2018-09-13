ed1c6ca15384d301b54fc610ff75f6697cf4a6e2
Daniel Santos <dfsantosbu@unal.edu.co>
2018-09-13 14:35:51 -0400

    Make progress in stuff.

---
+++ task/task1/exercise10/sol.m
@@ -0,0 +1,7 @@
+%{
+ author: Daniel Santos
+ date: 12 sep, 2018
+%}
+
+M = 1; B = 0.5; A = 0.5; g = 9.8;
+
--- task/task1/exercise9/sol.m
+++ task/task1/exercise9/sol.m
@@ -4,6 +4,12 @@
 format compact; clear all; close all;
 b0 = 0.005; b1 = 0.01; b2 = 0.01;
 
+num = [b0 b1 b2];
+den = [1 -1.8 0.82];
+
+G = tf(num, den)
+%stepplot(G);
+
 
 sys = newfis('fstate');
 
@@ -11,37 +17,40 @@ K = 1;
 %input error
 sys = addvar(sys,'input','error',[-1 1]);
  % activation function
- sys = addmf(sys,'input',1,'N','zmf',   [-0.938 -0.0301]);
+ sys = addmf(sys,'input',1,'N','zmf',   [-1 -0.0301]);
  sys = addmf(sys,'input',1,'Z','gaussmf',   [0.214 0]);
- sys = addmf(sys,'input',1,'P','smf',     [0.01807 0.881]);
+ sys = addmf(sys,'input',1,'P','smf',     [0.01807 1]);
  
 %input derivate error
 sys = addvar(sys,'input','derror',[-1 1]);
  % activation function
- sys = addmf(sys,'input',2,'N','zmf',   [-0.938 -0.0301]);
+ sys = addmf(sys,'input',2,'N','zmf',   [-1 -0.0301]);
  sys = addmf(sys,'input',2,'Z','gaussmf',   [0.214 0]);
- sys = addmf(sys,'input',2,'P','smf',     [0.01807 0.881]);
+ sys = addmf(sys,'input',2,'P','smf',     [0.01807 1]);
  
 
 %output
 sys = addvar(sys, 'output', 'rate', [-1 1]);
  % activation function 
- sys = addmf(sys,'output',1,'N','gaussmf',  [0.1705 -0.5]);
+ sys = addmf(sys,'output',1,'MN','gaussmf',   [0.171 -0.6133]   );
+ sys = addmf(sys,'output',1,'N','gaussmf',    [0.07584 -0.234]);
  sys = addmf(sys,'output',1,'Z','gaussmf',  [0.08016 0]);
- sys = addmf(sys,'output',1,'P','gaussmf',  [0.1705 0.5]);
+ sys = addmf(sys,'output',1,'P','gaussmf',   [0.065 0.2387]);
+ sys = addmf(sys,'output',1,'MP','gaussmf',   [0.171 0.7561]);
  
 %create rules
 % error, derror, output
+
  rules = [
-   1 1 1 1 1
-   1 2 1 1 1
-   1 3 1 1 1
-   2 1 1 1 1
-   2 2 2 1 1
-   2 3 3 1 1
-   3 1 3 1 1
-   3 2 3 1 1
-   3 3 3 1 1
+   1 1 5 1 1
+   1 2 4 1 1
+   1 3 4 1 1
+   2 1 4 1 1
+   2 2 3 1 1
+   2 3 2 1 1
+   3 1 2 1 1
+   3 2 2 1 1
+   3 3 1 1 1
  ];
 
 sys = addrule( sys, rules);
