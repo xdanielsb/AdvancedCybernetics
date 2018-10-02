9d0bd8a16e293b446068c6b77e06975e2a92b13b
Daniel Santos <dfsantosbu@unal.edu.co>
2018-10-02 18:47:06 -0400

    Solve exercise time series.

--- task/task2/exercise2/time.m
+++ task/task2/exercise2/time.m
@@ -2,20 +2,36 @@
  name: Daniel Santos
  date: Oct 2, 2018
 %}
-format compact; clear all; close all; clc;
+%format compact; clear all; close all; clc;
 
 offset_row = 1;
-unhealthy = csvread("./heart_.csv",offset_row);
-[nimages_un, ~]= size(unhealthy)
+heart = csvread("./heart_rate.csv",offset_row);
+[nrows, ~]= size(heart);
 
-plot( months , stock)
+sal = heart(:, 1)'; % heart rate
+from = 20;
+int = zeros(nrows,from);
+for i = from+1:nrows
+   for j = 1:from
+     int(i,j) = sal(i-j);
+   end
+end
+int = int(from+1:nrows,:);
+sal = sal(:,from+1:nrows);
+int  = int';
+nrows = nrows -from
 
+plot(1:nrows, sal); hold on;
 hiddenLayers = [2 2 2];
 net = feedforwardnet(hiddenLayers);
-net = train(net, months, stock);
-
+net = train(net, int , sal);
 %view (net);
-y = net( 15 )
-perf = perform( net, y, stock)
+
+y = net( int );
+perf = perform( net, y, sal)
+MSE=(1/nrows)*sum((sal-y).^2)
+
+plot(1:nrows, y)
+
 
 
