5e063432414df5aa947d1e907ed175491c26600f
Daniel Santos <dfsantosbu@unal.edu.co>
2019-01-31 09:55:21 -0500

    Implement predictive control neural network.

--- task/task2/exercise5/sys.m
+++ task/task2/exercise5/sys.m
@@ -9,18 +9,3 @@ close all; clc;
 k=4;b3=0;b2=2;b1=4;b0=2;
 
 
-hiddenLayers = [10 10 10 10];
-numdelay = 4;
-net = feedforwardnet(hiddenLayers);
-%O = O(7:57,:);
-net = train(net, I', O');
-
-%view (net);
-R = net(I');
-perf = perform( net, R, O')
-MSE=(1/51)*sum((R-O').^2)
-
-in = 1:51;
-plot(in, O'); hold on;
-plot(in, R)
-
