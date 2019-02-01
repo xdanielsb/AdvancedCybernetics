e327afe9a61ca34792efe93f59fcbd73cc341496
Daniel Santos <dfsantosbu@unal.edu.co>
2019-01-31 19:01:32 -0500

    Add solution exercise 6.

--- task/task2/exercise6/sys.m
+++ task/task2/exercise6/sys.m
@@ -6,4 +6,23 @@ format compact;
 %clear all;
 close all; clc; clear all;
 
-k=3;b3=2;b2=4;b1=3;b0=3;
+k = 3 ;
+b3 = 1 ;
+b2 = 4;
+b1 = 3;
+b0 = 3;
+
+
+
+
+
+
+
+
+
+k = 3*0.95 ;
+b3 = 0.5 ;
+b2 = 4;
+b1 = 3;
+b0 = 3;
+clc;
