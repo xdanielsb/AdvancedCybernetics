e4bd0806cba11c8baf622df25c6c17803a5e0fc4
Daniel Santos <dfsantosbu@unal.edu.co>
2019-02-05 09:59:44 -0500

    Improve solutions exercise 6,7,8.

--- task/task2/exercise8/Aplicacion.m
+++ task/task2/exercise8/Aplicacion.m
@@ -1,50 +1,48 @@
-%Ejemplo aplicaci�n ANFIS
-%ANFIS: Adaptive Neuro-Fuzzy training of Sugeno-type FIS.
-%Signal triangular a seno
- 
-%Simulacion del sistema
+%{
+ name: Daniel Santos
+ date: February 5th, 2019
+%}
+format compact; 
+%clear all;
+close all; clc; clear all;
+
+%Run simualation: Generate data
 sim('DatosSignals');
  
-%Se�al de entrada
+%Input & Output Signal
 x = simout(:,1);
- 
-%Se�al de salida
 y = simout(:,2);
  
-%Gr�fica de la se�al
-figure
-hold on
-plot(x,'r');
-plot(y,'b');
-hold off
+%Plot Signal
+figure; hold on; plot(x,'r');plot(y,'b');hold off
  
-%�pocas de entrenamiento 
+%Epochs
 epoch_n = 250;
  
-%Generaci�n del sistema difuso
-%in_fis  = genfis1([x y],5,'gbellmf','constant');
-in_fis  = genfis1([x y],5,'gbellmf','linear');
+%Fuzzy System
+opt = genfisOptions('GridPartition');
+opt.NumMembershipFunctions = [5];
+opt.InputMembershipFunctionType = ["gbellmf"];
+in_fis = genfis(x,y,opt);
+[x2,mf] = plotmf(in_fis,'input',1);
+plot(x2,mf)
+xlabel('input 1 (gaussmf)')
+
  
-%Entrenamiento del sistema difuso
+%Training fuzzy system
 out_fis = anfis([x y],in_fis,epoch_n);
  
 %Resultado del entrenamiento
 ys = evalfis(x,out_fis);
  
 %Resultados
-figure
-hold on
-plot(x,'r');
-plot(y,'b');
-plot(ys,'g');
-hold off
+figure;hold on; plot(x,'r'); plot(y,'b'); plot(ys,'g'); hold off
 legend('Triangular','Seno','ANFIS');
  
 %Figura del error
 e=y-ys;
-figure
-plot(e)
+%figure; plot(e)
  
-%Error cuadr�tico medio
+%Error mean cuadratic
 N = length(e);
 ECM = (1/N)*sum(e.^2)
